bd197ccdd21e0578720b667eec6cf481833b9b93
lostfly2 <lostfly2@naver.com>
2024-03-04 23:14:03 +0900

    회원 로직 개발

--- src/main/java/jpabook/jpashop2/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop2/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import jakarta.persistence.PersistenceContext;
 import jpabook.jpashop2.domain.Member;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public class MemberRepository {
 
@@ -21,4 +23,14 @@ public class MemberRepository {
         return em.find(Member.class, id);
     }
 
+    public List<Member> membersfindAll(){
+        return em.createQuery("select m from Member m",Member.class).getResultList();
+    }
+
+
+    public List<Member> findByname(String name){
+        return em.createQuery("select m from Member m where m.name= :name",Member.class)
+                .setParameter("name",name)
+                .getResultList();
+    }
 }
